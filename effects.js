ca4512322f0830f8cada4da4f22fdd92e490d79c
fahdalatfi <alatfifahd2@gmail.com>
2021-01-28 03:30:47 +0200

    adding js code in seperate file

---
+++ effects.js
@@ -0,0 +1,19 @@
+$('#templateCarousel').carousel()
+
+$(function() {
+  setTimeout(function() {
+    $(".caption1").addClass("fadein");
+  }, 100);
+});
+
+$(function() {
+  setTimeout(function() {
+    $(".display-2").addClass("fadein");
+  }, 100);
+});
+
+$(function() {
+    setTimeout(function() {
+      $(".card").addClass("scalein");
+    }, 100);
+  });
