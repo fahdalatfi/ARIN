762650122094d9f84fcabff63bf965feeda3da62
fahdalatfi <alatfifahd2@gmail.com>
2021-02-26 20:24:53 +0200

    updates

--- effects.js
+++ effects.js
@@ -1,7 +1,3 @@
-
-// Animating the carousel
-$('#templateCarousel').carousel()
-
 // Adding background to the navbar when scrolled
 $(window).scroll(function(){
     var scroll = $(window).scrollTop();
@@ -26,28 +22,3 @@ $(function() {
   }, 100);
 });
 
-// Cards fading animation
-$(function() {
-    setTimeout(function() {
-      $(".card").addClass("scalein");
-    }, 100);
-  });
-
-// Scroll down animation (being tested)
-  var $window = $(window);
-  var $elem = $(".card")
-  
-  function isScrolledIntoView($elem, $window) {
-      var docViewTop = $window.scrollTop();
-      var docViewBottom = docViewTop + $window.height();
-  
-      var elemTop = $elem.offset().top;
-      var elemBottom = elemTop + $elem.height();
-  
-      return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-  }
-  $(document).on("scroll", function () {
-      if (isScrolledIntoView($elem, $window)) {
-          $elem.addClass("scalein")
-      }
-  });
